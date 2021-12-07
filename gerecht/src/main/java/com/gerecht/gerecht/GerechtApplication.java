b0f5d7abdc706dc5fb7266cc4cfb734f6a6fb1d6
Enes52001 <enes.sahin@student.hu.nl>
2021-12-07 15:13:43 +0100

    rabbitmq tutorial kleine verandering en snel iets neergezet bij gerecht om te testen

--- gerecht/src/main/java/com/gerecht/gerecht/GerechtApplication.java
+++ gerecht/src/main/java/com/gerecht/gerecht/GerechtApplication.java
@@ -1,12 +1,31 @@
 package com.gerecht.gerecht;
 
+import com.gerecht.gerecht.domain.Gerecht;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @SpringBootApplication
 public class GerechtApplication {
 
     public static void main(String[] args) {
+
+        MongoClient client = MongoClients.create("mongodb://localhost");
+
+
+        MongoOperations mongoOps = new MongoTemplate(client, "database");
+        mongoOps.insert(new Gerecht());
+
+        System.out.println(mongoOps.findOne(new Query(where("name").is("spaghetti")), Gerecht.class));
+
+        mongoOps.dropCollection("someEntity");
+
         SpringApplication.run(GerechtApplication.class, args);
     }
 
---
+++ gerecht/src/main/java/com/gerecht/gerecht/domain/Gerecht.java
@@ -0,0 +1,8 @@
+package com.gerecht.gerecht.domain;
+
+public class Gerecht {
+
+    private boolean verkocht;
+    private String naam;
+
+}
